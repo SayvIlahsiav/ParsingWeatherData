d33973a7321aec08a440221303ee21d5be009010
Vaishali Vyas <sayvilahsiav@outlook.com>
2023-11-04 06:51:53 +0530

    Added methods finding lowest humidity
    Find lowest humidity hour in file and find lowest humidity day in files.

--- ParsingWeatherData.java
+++ ParsingWeatherData.java
@@ -12,16 +12,22 @@ import java.io.*;
 
 public class ParsingWeatherData 
 {
-    public CSVRecord lowestOfTwo(CSVRecord currentRow, CSVRecord lowestSoFar)
+    public CSVRecord lowestOfTwo(CSVRecord currentRow, CSVRecord lowestSoFar, String column)
     {
         if (lowestSoFar == null)
+        {
+            if (!currentRow.get(column).equals("N/A")) 
                 lowestSoFar = currentRow;
+        }
         else
         {
-            double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
-            double lowestTemp = Double.parseDouble(lowestSoFar.get("TemperatureF"));
-            if(currentTemp < lowestTemp && currentTemp != -9999)
-                lowestSoFar= currentRow;
+            if(!currentRow.get(column).equals("N/A"))
+            {
+                double current = Double.parseDouble(currentRow.get(column));
+                double lowest = Double.parseDouble(lowestSoFar.get(column));
+                if(current < lowest && current != -9999)
+                    lowestSoFar= currentRow;
+            }
         }
         return lowestSoFar;
     }
@@ -31,7 +37,7 @@ public class ParsingWeatherData
         CSVRecord lowestSoFar = null;
         for (CSVRecord currentRow : parser)
         {
-            lowestSoFar = lowestOfTwo(currentRow, lowestSoFar);
+            lowestSoFar = lowestOfTwo(currentRow, lowestSoFar, "TemperatureF");
         }
         return lowestSoFar;
     }
@@ -46,9 +52,33 @@ public class ParsingWeatherData
             fileName = f.getName();
             FileResource fr = new FileResource(f);
             CSVRecord currentRow = coldestHourInFile(fr.getCSVParser());
-            lowestSoFar = lowestOfTwo(currentRow, lowestSoFar);
+            lowestSoFar = lowestOfTwo(currentRow, lowestSoFar, "TemperatureF");
         }
         return fileName;
+    }
+    
+    public CSVRecord lowestHumidityInFile(CSVParser parser)
+    {
+        CSVRecord lowestSoFar = null;
+        for (CSVRecord currentRow : parser)
+        {
+            lowestSoFar = lowestOfTwo(currentRow, lowestSoFar, "Humidity");
+        }
+        return lowestSoFar;
+    }
+    
+    public CSVRecord lowestHumidityInFiles()
+    {
+        CSVRecord lowestSoFar = null;
+        DirectoryResource dr = new DirectoryResource();
+        String fileName = null;
+        for (File f : dr.selectedFiles())
+        {
+            FileResource fr = new FileResource(f);
+            CSVRecord currentRow = lowestHumidityInFile(fr.getCSVParser());
+            lowestSoFar = lowestOfTwo(currentRow, lowestSoFar, "Humidity");
+        }
+        return lowestSoFar;
     }
     
     public void testColdestHourInFile()
@@ -78,10 +108,32 @@ public class ParsingWeatherData
         }
     }
     
+    public void testLowestHumidityInFile()
+    {
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser();
+        CSVRecord lowest = lowestHumidityInFile(parser);
+        System.out.println("Lowest Humidity was " 
+                            + lowest.get("Humidity") 
+                            + " at " 
+                            + lowest.get("DateUTC"));
+    }
+    
+    public void testLowestHumidityInFiles()
+    {
+        CSVRecord lowest = lowestHumidityInFiles();
+        System.out.println("Lowest Humidity was " 
+                            + lowest.get("Humidity") 
+                            + " at " 
+                            + lowest.get("DateUTC"));
+    }
+    
     public static void main(String[] args)
     {
         ParsingWeatherData pwd1 = new ParsingWeatherData();
         //pwd1.testColdestHourInFile();
-        pwd1.testColdestDayInFiles();
+        //pwd1.testColdestDayInFiles();
+        //pwd1.testLowestHumidityInFile();
+        pwd1.testLowestHumidityInFiles();
     }
 }
